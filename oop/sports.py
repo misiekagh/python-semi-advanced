83d8d6b67e006a409645f3ec0945633c61342f4c
Misiek <misiekt@gmail.com>
2019-07-17 15:36:45 +0200

    Initial commit

--- oop/sports.py
+++ oop/sports.py
@@ -26,6 +26,26 @@ class Arena:
         self.games.append(game)
         return game
 
+    ''' stworz liste zawodnikow 2. stworz mape liczba zwycies
+    3. posortuj'''
+    def standing1(self, playes):
+        result = []
+        players = []
+        for game in self.games:
+            if game.white not in players:
+                players.append(game.white)
+                scores.append(0)
+            if game.black not in players:
+                players.append(game.black)
+                scores.append(0)
+            if game.white_won():
+                index=players.index(game.white)
+            else:
+                index = players.index(black)
+            scores[index]+=1
+        player_score_list=list(zip(players, scores))
+        player_score_list.sort(key=lambda x: x[1], reverse=True)
+        return [pair[0] ]
 
 @dataclass
 class Game:
